eb14ee12c42e1ce6ff9d0fcc99c733b89132fc94
Victoria Kazeem <victoriakazeem@MACs-MacBook-Air.local>
2020-02-26 16:44:44 +0100

    created a file sermons.js to manage all sermons

---
+++ website/sermons.js
@@ -0,0 +1,71 @@
+let sermons = [
+    {
+        title: "Implicit Obedience",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "God needs leaders",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "Enthrone him as king",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "How does God see you",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+    },
+    {
+        title: "",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    },
+    {
+        title: "",
+        pastor: "",
+        text_content: "",
+        audio_url: ""
+
+    }
+]
